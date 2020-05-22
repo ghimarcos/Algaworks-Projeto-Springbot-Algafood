fedaf9bccda7c98431806b5e0168c8bda7c030d5
Giovani Ribeiro <ghimarcos123@gmail.com>
2020-05-22 10:42:57 -0300

    Adicionado ao Controller da Cozinha, um método para retornar uma busca por apenas um elemento passando o ID do elemento

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,14 @@ public class CozinhaController {
 	@GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}) /*Mapeamento do verbo http 'GET' + afirmando para apenas retornar em JsonS ou XML*/
 	public List<Cozinha> listar(){
 		return cozinhaRepository.listar();  /*Passo o modo listar do CozinhaRepository para o controlador*/
+	}
+	
+	@GetMapping("/{cozinhaId}") /*aqui estou dizendo o caminho do get tem que ser em /cozinhas/{cozinhaId}*/
+	
+	public Cozinha buscar(@PathVariable Long cozinhaId) {   /*Aqui com o @PathVariable estou dizendo que o ID que ele vai 
+	receber para fazer a busca vai ser o ID que eu passar no GET*/
+		
+		return cozinhaRepository.buscar(cozinhaId);  /*Metodo buscar por ID*/
 	}
 	
 }
