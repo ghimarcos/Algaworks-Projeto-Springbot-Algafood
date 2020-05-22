595753195cbdb48a2aae5f5fb4f17940b0c332f9
Giovani Ribeiro <ghimarcos123@gmail.com>
2020-05-22 11:02:37 -0300

    Entendendo sobre algumas anotaçoes do @Json, mudando os nomes do request, ou ignorar um valor

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -8,11 +8,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 
-
+@JsonRootName("gastronomia")  /*Essa anotação está dizendo para não retornar Cozinha no request, e sim "gastronomia" só funciona no
+XML, o Json não retorna nome root*/
 @Data /*Lombok , cria setters, getters automatico*/
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity /*Dizendo que essa classe é gerenciavel pelo Spring*/
@@ -23,7 +28,9 @@ public class Cozinha {
 	@Id /* Nessa tabela esse elemento vai ser o ID */
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // *esse código diz que o ID tem que ser Auto Increment*//
 	private Long id;
-
+	
+//	@JsonIgnore /*A anotação JsonIgnore, diz que é pra ignorar a String Nome, e não retornar seu valor*/
+	@JsonProperty("titulo")   /*Aqui estou dizendo que no resquest não vai retornar mais nome, e sim titulo*/
 	@Column(name = "nom_cozinha", length = 30) // **coluna no banco com tamanho de 30 varchar**//
 	private String nome;
 
